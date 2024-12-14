750721953b92372ad813351cfbd72682f6413e87
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-12-13 23:30:22 -0600

    Create HomepageHero component and integrate it into App.jsx

--- trove-track-ui/src/App.jsx
+++ trove-track-ui/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import OffcanvasNavbar from './components/Navbar/OffcanvasNavbar';
+import HomepageHero from './components/HomepageHero/HomepageHero';
 
 function App() {
 
   return (
     <>
       <OffcanvasNavbar />
+      <HomepageHero />
     </>
   )
 }
---
+++ trove-track-ui/src/components/HomepageHero/HomepageHero.jsx
@@ -0,0 +1,20 @@
+import './homepage-hero.css';
+
+const HomepageHero = () => {
+  return (
+    <div id="homepageHeroBackground" className="p-5">
+      <div className="container homepage-hero-text">
+        <h1 className="display-5 fw-bold">Welcome to TroveTrack.</h1>
+        <h2></h2>
+        <p className="col-md-8 fs-4 py-2">Manage your inventory with ease using our straightforward tool. Track stock levels, create new invoices, and stay on top of low stock items — all in one place. We will help you keep your inventory organized and your business running smoothly. Inventory management made easy!</p>
+        <button className="btn-navyblue" type="button">
+          <span>
+            Sign In
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default HomepageHero;
