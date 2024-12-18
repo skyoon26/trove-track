0102caa91b0511260bddf2734bd42904bb48eb59
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-12-18 11:13:37 -0600

    Add logic to handle category creation in CategoryService using CategoryRepository

--- trove-track-backend/src/main/java/com/trovetrack/service/CategoryService.java
+++ trove-track-backend/src/main/java/com/trovetrack/service/CategoryService.java
@@ -1,2 +1,20 @@
-package com.trovetrack.service;public class CategoryService {
+package com.trovetrack.service;
+
+import com.trovetrack.entity.Category;
+import com.trovetrack.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category) {
+        category.setDateCreated(LocalDateTime.now());
+        return categoryRepository.save(category);
+    }
 }
