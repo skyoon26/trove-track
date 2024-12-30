84a7814c36b423f8308a1f270258d453d8e08936
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-12-30 11:11:55 -0600

    Implement CustomUserDetailsService with methods for loading user data and mapping roles to authorities

---
+++ trove-track-backend/src/main/java/com/trovetrack/security/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.trovetrack.security;
+
+import com.trovetrack.entity.Role;
+import com.trovetrack.entity.UserEntity;
+import com.trovetrack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service // This class loads user-specific data for authentication and authorization
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired // Constructor with userRepository dependency injection
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override // This is a built-in method part of the UserDetailsService (Spring Security interface), it is used to fetch the user's data for authentication
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    // This method converts a list of roles (ADMIN, USER) into Spring Security authorities to manage access control
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+}
