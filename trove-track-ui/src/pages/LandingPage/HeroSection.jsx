dcf0808c535022929b176002b99c86bd89944497
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-12-30 15:36:38 -0600

    Rename LandingPageHero and CSS file to HeroSection for clarity

--- trove-track-ui/src/pages/LandingPage/HeroSection.jsx
+++ trove-track-ui/src/pages/LandingPage/HeroSection.jsx
@@ -1,15 +1,15 @@
-import './landing-page-hero.css';
+import './hero-section.css';
 
-const LandingPageHero = () => {
+const HeroSection = () => {
   return (
-    <div className="p-5 homepage-hero-section">
-      <div className="container homepage-hero-text">
+    <div className="p-5 hero-section">
+      <div className="container">
         <h1 className="display-5 fw-bold">Welcome to TroveTrack.</h1>
         <h2></h2>
         <p className="col-md-8 fs-4 py-2">Manage your inventory with ease using our straightforward tool. Track stock levels, create new invoices, and stay on top of low stock items — all in one place. We will help you keep your inventory organized and your business running smoothly. Inventory management made easy!</p>
         <button className="btn-navyblue" type="button">
           <span>
-            Sign In
+            Sign in
           </span>
         </button>
       </div>
@@ -17,4 +17,4 @@ const LandingPageHero = () => {
   );
 }
 
-export default LandingPageHero;
+export default HeroSection;
