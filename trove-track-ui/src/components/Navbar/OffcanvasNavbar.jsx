223a6ae6f92e7a7881448600166b2868909558b3
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-05 17:23:51 -0600

    Add navigation links to OffcanvasNavbar component and improve styling

--- trove-track-ui/src/components/Navbar/OffcanvasNavbar.jsx
+++ trove-track-ui/src/components/Navbar/OffcanvasNavbar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Navbar, Container, Offcanvas, Nav } from 'react-bootstrap';
+import { Navbar, Container, Offcanvas, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './offcanvas-navbar.css';
 
 const OffcanvasNavbar = () => {
@@ -22,35 +23,54 @@ const OffcanvasNavbar = () => {
   }, []);
 
   return (
-    <Navbar expand={false} sticky="top" className={`offcanvas-navbar-section ${scrolled ? 'scrolled' : ''}`}>
-      <Container fluid>
-        <div className="d-flex align-items-center justify-content-between w-100">
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Brand href="#" className='d-none d-md-inline mx-2'>Menu</Navbar.Brand>
-          <div className="text-center flex-grow-1">
-            <Navbar.Brand id="troveTrackNavLink" href="#">TroveTrack</Navbar.Brand>
+    <>
+      <Navbar expand={false} sticky="top" className={`offcanvas-navbar-section ${scrolled ? 'scrolled' : ''}`}>
+        <Container fluid>
+          <div className="d-flex align-items-center justify-content-between w-100">
+            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <Navbar.Brand 
+              href="#" 
+              className='d-none d-md-inline mx-2'
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+            >
+              Menu
+            </Navbar.Brand>
+            <Navbar.Brand
+              id="troveTrackNavLink"
+              className="mx-auto text-center"
+            >
+              <Nav.Link as={Link} to={"/landing"}>TroveTrack</Nav.Link>
+            </Navbar.Brand>
           </div>
-        </div>
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="start"
-          backdrop={false}
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">
-              Offcanvas Menu
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="start"
+            backdrop={true}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                Offcanvas Menu
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                <Nav.Link as={Link} to={"/order"}>Order</Nav.Link>
+                <Nav.Link as={Link} to={"/manage"}>Manage</Nav.Link>
+                <Nav.Link as={Link} to={"/search"}>Search</Nav.Link>
+                <NavDropdown title="Account" id="collapsible-nav-dropdown">
+                  <NavDropdown.Item as={Link} to={"/account"}>View</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to={"/account"}>Sign out</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
