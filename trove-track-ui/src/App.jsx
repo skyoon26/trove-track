e90126acfcae3bd54a4530ff18018aa3501a5060
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-05 17:20:17 -0600

    Set up navigation routes for Landing, Home, Order, Manage, Search, and Account pages

--- trove-track-ui/src/App.jsx
+++ trove-track-ui/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import './App.css'
 import OffcanvasNavbar from './components/Navbar/OffcanvasNavbar';
 import LandingPage from './pages/LandingPage/LandingPage';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import OrderPage from './pages/OrderPage/OrderPage';
+import ManagePage from './pages/ManagePage/ManagePage';
+import SearchPage from './pages/SearchPage/SearchPage';
+import AccountPage from './pages/AccountPage/AccountPage';
 
 function App() {
 
   return (
     <>
       <OffcanvasNavbar />
-      <LandingPage />
+      <Routes>
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/manage" element={<ManagePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
     </>
   )
 }
