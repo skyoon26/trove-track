6068bd87061a881d28e6d760ec6984d1617d3b5b
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-06 11:14:46 -0600

    Set up user registration endpoint in AuthController

--- trove-track-backend/src/main/java/com/trovetrack/controller/AuthController.java
+++ trove-track-backend/src/main/java/com/trovetrack/controller/AuthController.java
@@ -1,13 +1,18 @@
 package com.trovetrack.controller;
 
+import com.trovetrack.dto.RegisterDto;
+import com.trovetrack.entity.Role;
+import com.trovetrack.entity.UserEntity;
 import com.trovetrack.repository.RoleRepository;
 import com.trovetrack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,5 +31,28 @@ public class AuthController {
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
-    
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        // User validation
+        if(userRepository.existsByUsername(registerDto.getUsername())) {
+            return new ResponseEntity<>("Username already in use", HttpStatus.BAD_REQUEST);
+        }
+
+        // If the username is not found in the database, create and save a new User entity
+        UserEntity user = new UserEntity();
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+        user.setFirstName(registerDto.getFirstName());
+        user.setLastName(registerDto.getLastName());
+        user.setEmail(registerDto.getEmail());
+
+        // Fetches the "USER" role from the database and assigns it to the User
+        Role roles = roleRepository.findByName("USER").get();
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully!", HttpStatus.OK);
+    }
 }
