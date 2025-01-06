2b086a273697920790bf962b3eed56816f19d9de
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-06 10:36:55 -0600

    Add AuthController with fields and constructor for user registration and login

---
+++ trove-track-backend/src/main/java/com/trovetrack/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.trovetrack.controller;
+
+import com.trovetrack.repository.RoleRepository;
+import com.trovetrack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173/")
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    
+}
