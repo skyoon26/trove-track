571f9f11d9b47c7d6a227ba865d4806808f84db4
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-13 23:38:49 -0600

    Move HeroSection.jsx code into LandingPage.jsx and rename CSS file to landing-page.css to reduce unnecessary nesting

--- trove-track-ui/src/pages/LandingPage/LandingPage.jsx
+++ trove-track-ui/src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import HeroSection from './HeroSection'
+import { useNavigate } from "react-router-dom";
+import './landing-page.css';
+import chest from "../../assets/images/chest.png"
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    navigate("/signin");
+  };
+
   return (
-    <HeroSection />
-  )
+    <div className="hero-section p-4">
+      <div className="container d-flex flex-column flex-lg-row align-items-center">
+        <div className="text-content">
+          <h1 className="display-5 fw-bold">Welcome to TroveTrack.</h1>
+          <p className="col-md-10 fs-4 py-2">Manage your inventory with ease using our straightforward tool. Track stock levels, create new invoices, and stay on top of low stock items — all in one place. We will help you keep your inventory organized and your business running smoothly. Inventory management made easy!</p>
+          <button className="btn-navyblue" type="button" onClick={handleSignInClick}>
+            <span>
+              Sign in
+            </span>
+          </button>
+        </div>
+        
+        <div className="image-content">
+          <img src={chest} alt="Treasure chest representing organized inventory with TroveTrack"/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
