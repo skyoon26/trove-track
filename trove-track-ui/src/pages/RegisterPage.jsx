8749f9be645d20d179ce67cf32c4e06a7cc678ef
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-16 16:56:56 -0600

    Set up state and handler functions for submitting registration form in RegisterPage

--- trove-track-ui/src/pages/RegisterPage.jsx
+++ trove-track-ui/src/pages/RegisterPage.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { register } from '../services/authService';
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: ''
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage('');  // Reset previous error
+    setSuccessMessage('');  // Reset previous success
+
+    const result = await register(formData);
+
+    if (result === 'User registered successfully!') {
+      setSuccessMessage(result);
+    } else {
+      setErrorMessage(result);
+    }
+  };
+
   return (
     <>
       <Container>
@@ -12,49 +45,68 @@ const RegisterPage = () => {
               <Card.Body>
                 <h1 className="text-center">Let's Get Started!</h1>
                 <p className="text-center">Please enter your details below</p>
-                <Form className="mb-4">
-                  <Form.Group className="my-4" controlId="firstName">
-                    <Row>
+                {errorMessage && <p className="text-danger text-center">{errorMessage}</p>}
+                {successMessage && <p className="text-success text-center">{successMessage}</p>}
+                <Form className="mb-4" onSubmit={handleSubmit}>
+                    <Row className="my-4" >
                       <Col>
-                        <Form.Control
-                          type="text"
-                          placeholder="Enter first name"
-                          required
-                          autoComplete='off'
-                        />
+                        <Form.Group controlId="firstName">
+                          <Form.Control
+                            type="text"
+                            name="firstName" 
+                            placeholder="Enter first name"
+                            autoComplete='off'
+                            value={formData.firstName}
+                            onChange={handleChange}
+                            required
+                          />
+                        </Form.Group>
                       </Col>
                       <Col>
-                        <Form.Control
-                          type="text"
-                          placeholder="Enter last name"
-                          required
-                          autoComplete='off'
-                        />
+                        <Form.Group controlId="lastName">
+                          <Form.Control
+                            type="text"
+                            name="lastName"
+                            placeholder="Enter last name"
+                            autoComplete='off'
+                            value={formData.lastName}
+                            onChange={handleChange}
+                            required
+                          />
+                        </Form.Group>
                       </Col>
                     </Row>
-                  </Form.Group>
-                  <Form.Group className="my-4" controlId="email">
+                  <Form.Group controlId="email">
                     <Form.Control
                       type="email"
+                      name="email"
                       placeholder="Enter email"
-                      required
                       autoComplete='off'
+                      value={formData.email}
+                      onChange={handleChange}
+                      required
                     />
                   </Form.Group>
                   <Form.Group className="my-4" controlId="username">
                     <Form.Control
                       type="text"
+                      name="username"
                       placeholder="Enter username"
-                      required
                       autoComplete='off'
+                      value={formData.username}
+                      onChange={handleChange}
+                      required
                     />
                   </Form.Group>
                   <Form.Group className="my-4" controlId="password">
                     <Form.Control
                       type="password"
+                      name="password"
                       placeholder="Enter password"
-                      required
                       autoComplete='off'
+                      value={formData.password}
+                      onChange={handleChange}
+                      required
                     />
                   </Form.Group>
                   <div className="d-flex justify-content-center">
@@ -75,4 +127,4 @@ const RegisterPage = () => {
   )
 }
 
-export default RegisterPage
+export default RegisterPage;
