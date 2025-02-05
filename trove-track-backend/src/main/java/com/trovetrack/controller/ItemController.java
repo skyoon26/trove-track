a8240c13a9d2bcda32d99b860e6d9c3bf5a27056
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-05 16:04:05 -0600

    Correct commit message to reflect actual CRUD endpoints for ItemController

--- trove-track-backend/src/main/java/com/trovetrack/controller/ItemController.java
+++ trove-track-backend/src/main/java/com/trovetrack/controller/ItemController.java
@@ -1,11 +1,15 @@
 package com.trovetrack.controller;
 
+import com.trovetrack.dto.ItemDto;
 import com.trovetrack.service.CategoryService;
 import com.trovetrack.service.ItemService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -17,8 +21,51 @@ public class ItemController {
 
     private final CategoryService categoryService;
 
-//    @PostMapping("/items")
-//    public ResponseEntity<ItemDto> createItem(@RequestBody ItemDto itemDto) {
-//
-//    }
+    @PostMapping("/items")
+    public ResponseEntity<ItemDto> createItem(@RequestBody ItemDto itemDto) {
+        ItemDto newItem = itemService.createItem(itemDto);
+
+        return new ResponseEntity<>(newItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<ItemDto>> getAllItems() {
+        List<ItemDto> items = itemService.getAllItems();
+
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+    @GetMapping("/items/{id}")
+    public ResponseEntity<?> getItemById(@PathVariable int id) {
+        try {
+            ItemDto itemDto = itemService.getItemById(id);
+
+            return ResponseEntity.ok(itemDto);
+        } catch (EntityNotFoundException e) {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/items/{id}")
+    public ResponseEntity<?> updateItem(@PathVariable int id, @RequestBody ItemDto itemDto) {
+        try {
+            ItemDto updatedItem = itemService.updateItem(id, itemDto);
+
+            return ResponseEntity.ok(updatedItem);
+        } catch (EntityNotFoundException e) {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/items/{id}")
+    public ResponseEntity<?> deleteItem(@PathVariable int id) {
+        try {
+            itemService.deleteItem(id);
+            return ResponseEntity.ok("Item with ID " + id + " deleted successfully");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
