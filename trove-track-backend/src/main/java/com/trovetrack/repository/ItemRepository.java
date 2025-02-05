062217c2edfeab79bdf96c6bf25c21e17bdc9850
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-05 00:12:48 -0600

    Add repository layers for Item, OrderItem, and Order entities

---
+++ trove-track-backend/src/main/java/com/trovetrack/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.trovetrack.repository;
+
+import com.trovetrack.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+}
---
+++ trove-track-backend/src/main/java/com/trovetrack/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.trovetrack.repository;
+
+import com.trovetrack.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
