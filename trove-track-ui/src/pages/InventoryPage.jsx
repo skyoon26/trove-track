e1b8d06894a4ae26ee64a8dc49d05a6161336067
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-12 17:00:39 -0600

    Set up initial layout for InventoryPage

--- trove-track-ui/src/pages/InventoryPage.jsx
+++ trove-track-ui/src/pages/InventoryPage.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import { Container, Stack, Card, Button, Accordion } from 'react-bootstrap';
+import PageTabs from '../components/PageTabs';
 
 const InventoryPage = () => {
+  const firstName = sessionStorage.getItem("firstName");
+
+  const today = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
-    <h1>Inventory</h1>
+    <Container className="main-container py-3 m-5">
+      <Stack direction="horizontal" gap={3} className="m-2">
+        <h2 className="p-2">{firstName ? firstName : "Guest's"}'s Inventory</h2>
+        <p className="p-2 ms-auto">{today}</p>
+      </Stack>
+
+      <div>
+        <Card className="px-2 mt-4">
+          <Stack direction="horizontal" gap={3}>
+            <h3 className="p-2">Category Name</h3>
+            <Button variant="outline-primary" className="p-2 ms-auto">Add Category</Button>
+            <Button variant="outline-primary" className="p-2">Add Item</Button>
+          </Stack>
+        </Card>
+        <Accordion alwaysOpen className="pt-2">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Accordion Item #3</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+      <PageTabs />
+    </Container>
   )
 }
 
