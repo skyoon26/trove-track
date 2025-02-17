8b9d309fd3c41b47a30d458eb13af6e92ca65599
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-16 20:47:42 -0600

    Add GET request with auth for category retrieval in categoryService

--- trove-track-ui/src/services/categoryService.js
+++ trove-track-ui/src/services/categoryService.js
@@ -2,22 +2,35 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/categories";
 
+const token = sessionStorage.getItem("authToken");
+
 // Create a new category
 export const createCategory = async (categoryName) => {
-  const token = sessionStorage.getItem("authToken");
   try {
     const response = await axios.post(
       API_URL, 
       { name: categoryName },
-      {
-        headers: {
+      { headers: {
           Authorization: token,
-        }
-      }
-    );
+      }});
     return response.data;
   } catch (error) {
     console.error("Error creating category: ", error);
+    throw error;
+  }
+};
+
+// Fetch all categories
+export const getAllCategories = async () => {
+  try {
+    const response = await axios.get(
+      API_URL,
+      { headers: {
+        Authorization: token,
+    }});
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching categories: ", error);
     throw error;
   }
 };
