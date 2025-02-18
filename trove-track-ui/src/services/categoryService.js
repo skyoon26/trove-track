d836383824fba8f47f8ba252ac9d36f591f3ea7c
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-17 19:38:33 -0600

    Refactor token retrieval in categoryService to be function-based instead of using a variable

--- trove-track-ui/src/services/categoryService.js
+++ trove-track-ui/src/services/categoryService.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/categories";
 
-const token = sessionStorage.getItem("authToken");
-
 // Create a new category
 export const createCategory = async (categoryName) => {
+  const token = sessionStorage.getItem("authToken");
   try {
     const response = await axios.post(
       API_URL, 
       { name: categoryName },
       { headers: {
           Authorization: token,
-      }});
+      }}
+    );
     return response.data;
   } catch (error) {
     console.error("Error creating category: ", error);
@@ -22,12 +22,14 @@ export const createCategory = async (categoryName) => {
 
 // Fetch all categories
 export const getAllCategories = async () => {
+  const token = sessionStorage.getItem("authToken");
   try {
     const response = await axios.get(
       API_URL,
       { headers: {
         Authorization: token,
-    }});
+      }}
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching categories: ", error);
