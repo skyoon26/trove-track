6b570e2f7f13b96abefc52a75235512a617d14aa
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-17 19:33:21 -0600

    Set up state, hooks, and form handling for item creation POST request in InventoryPage

--- trove-track-ui/src/pages/InventoryPage.jsx
+++ trove-track-ui/src/pages/InventoryPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
-import { Container, Stack, Card, Button, Accordion, Modal, InputGroup, Form, Table } from 'react-bootstrap';
+import { Container, Stack, Card, Button, Accordion, Modal, InputGroup, Form, Table, Row, Col } from 'react-bootstrap';
 import { createCategory, getAllCategories } from '../services/categoryService';
+import { createItem } from '../services/itemService';
 import PageTabs from '../components/PageTabs';
 
 const InventoryPage = () => {
@@ -22,7 +23,23 @@ const InventoryPage = () => {
     setSuccess(false);
   };
 
-  // Manages state and handlers for the "Add Category" button and modal
+  // Manages category state, fetches category data from API, and updates UI
+  const [categories, setCategories] = useState([]);
+  
+  const fetchCategories = async () => {
+    try {
+      const data = await getAllCategories();
+      setCategories(data);
+    } catch (error) {
+      setError("Oops! We couldn't fetch the categories. Please try again.");
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  // Manages state and handlers for the "Add Category" and "Add Item" buttons and modals
   const [showCategoryModal, setShowCategoryModal] = useState(false);
   const handleCloseCategoryModal = () => {
     setShowCategoryModal(false);
@@ -30,6 +47,24 @@ const InventoryPage = () => {
     resetMessages();
   };
   const handleShowCategoryModal = () => setShowCategoryModal(true);
+
+  const [showItemModal, setShowItemModal] = useState(false);
+  const handleCloseItemModal = () => {
+    setShowItemModal(false);
+    setItemData({
+      name: "",
+      quantity: "",
+      minQuantity: "",
+      price: "",
+      location: "",
+      description: "",
+      categoryId: "",
+      amazonProductId: "",
+      image: "",
+    });
+    resetMessages();
+  };
+  const handleShowItemModal = () => setShowItemModal(true);
 
   // Initializes state for category name
   const [categoryName, setCategoryName] = useState("");
@@ -53,30 +88,65 @@ const InventoryPage = () => {
     await handleAddCategory(categoryName);
   };
 
+  // Initializes state for item
+  const [itemData, setItemData] = useState({
+    categoryId: "",
+    name: "",
+    quantity: "",
+    minQuantity: "",
+    price: "",
+    location: "",
+    description: ""
+  });
+
+  // Handles item addition
+  const handleAddItem = async (itemData) => {
+    setSuccess(false);
+    setError(null);
+    try {
+      await createItem(itemData);
+      setSuccess(true);
+      fetchCategories(); // Re-fetches the categories to display the updated data
+    } catch (error) {
+      setError("Oops! We couldn't add the new item. Please try again later.");
+      setSuccess(false);
+    }
+  };
+
+  // Updates the state for a specific value in itemData
+  const handleItemChange = e => {
+    const { name, value } = e.target;
+    setItemData({
+      ...itemData,
+      [name]: value,
+    });
+  };
+
+  // Handles form submission for adding a new item
+  const handleSubmitItem = async e => {
+    e.preventDefault();
+
+    const item = {
+      categoryId: itemData.categoryId,
+      name: itemData.name,
+      quantity: itemData.quantity,
+      minQuantity: itemData.minQuantity,
+      price: itemData.price,
+      location: itemData.location,
+      description: itemData.description,
+    };
+    await handleAddItem(item);
+  };
+
   // Closes modal after successful form submission
   useEffect(() => {
     if (success) {
       setTimeout(() => {
         handleCloseCategoryModal();
+        handleCloseItemModal();
       }, 1000);
     }
   }, [success]);
-
-  // Manages category state, fetches category data from API, and updates UI
-  const [categories, setCategories] = useState([]);
-  
-  const fetchCategories = async () => {
-    try {
-      const data = await getAllCategories();
-      setCategories(data);
-    } catch (error) {
-      setError("Oops! We couldn't fetch the categories. Please try again.");
-    }
-  };
-
-  useEffect(() => {
-    fetchCategories();
-  }, []);
 
   return (
     <Container className="main-container py-3">
@@ -123,7 +193,114 @@ const InventoryPage = () => {
             </Modal.Footer>
           </Modal>
 
-          <Button variant="outline-primary" className="p-2">New Item</Button>
+          <Button variant="outline-primary" className="p-2" onClick={handleShowItemModal}>New Item</Button>
+          <Modal size="lg" centered show={showItemModal} onHide={handleCloseItemModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>New Item</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Form id="addItemForm" onSubmit={handleSubmitItem}>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="itemName">
+                  <Form.Label>Item Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    placeholder="Enter name"
+                    value={itemData.name}
+                    onChange={handleItemChange}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="itemCategory">
+                  <Form.Label>Category</Form.Label>
+                  <Form.Select
+                    name="categoryId"
+                    value={itemData.categoryId}
+                    onChange={handleItemChange}
+                  >
+                    <option value="">Select...</option>
+                    {categories.map(category => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="itemQuantity">
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="quantity"
+                    placeholder="Enter quantity"
+                    value={itemData.quantity}
+                    onChange={handleItemChange}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="itemMinQuantity">
+                  <Form.Label>Minimum Quantity</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="minQuantity"
+                    placeholder="Enter minimum quantity"
+                    value={itemData.minQuantity}
+                    onChange={handleItemChange}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="itemPrice">
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="price"
+                    placeholder="Enter price"
+                    value={itemData.price}
+                    onChange={handleItemChange}
+                  />
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="itemLocation">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="location"
+                    placeholder="Enter location"
+                    value={itemData.location}
+                    onChange={handleItemChange}
+                  />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3" controlId="itemDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={2}
+                  name="description"
+                  placeholder="Enter description"
+                  value={itemData.description}
+                  onChange={handleItemChange}
+                />
+              </Form.Group>
+              {error && <div className='alert alert-danger'>{error}</div>}
+              {success && <div className='alert alert-success'>New item added successfully!</div>}
+              </Form>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleCloseItemModal}>
+                Close
+              </Button>
+              <Button variant="primary" type="submit" form="addItemForm">
+                Submit
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Stack>
       </Card>
 
