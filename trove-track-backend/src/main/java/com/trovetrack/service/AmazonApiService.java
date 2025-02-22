15fea6f0f7741e008a5fe94c59b62463e154e186
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-22 12:03:27 -0600

    Add AmazonApiService to fetch product details from Amazon API

---
+++ trove-track-backend/src/main/java/com/trovetrack/service/AmazonApiService.java
@@ -0,0 +1,34 @@
+package com.trovetrack.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AmazonApiService {
+    @Value("${rapidapi.amazon.url}")
+    private String apiUrl;
+    @Value("${rapidapi.amazon.key}")
+    private String apiKey;
+    private final RestTemplate restTemplate;
+    public AmazonApiService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public String fetchAmazonProduct(String productId) {
+        String url = apiUrl + "/product-details?asin=" + productId + "&country=US";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Key", apiKey);
+        headers.set("X-RapidAPI-Host", "real-time-amazon-data.p.rapidapi.com");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        return response.getBody();
+    }
+}
