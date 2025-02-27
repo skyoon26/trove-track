575c8ff25fd9afc62ff76946a9479435cf4fc221
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-27 14:22:17 -0600

    Refactor HistoryPage to include modal form for new inventory logs

--- trove-track-ui/src/pages/HistoryPage.jsx
+++ trove-track-ui/src/pages/HistoryPage.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Stack, Card, Button, Accordion, Table } from 'react-bootstrap';
+import { Container, Stack, Card, Button, Accordion, Table, Modal, Form, Row, Col } from 'react-bootstrap';
+import { getAllCategories } from "../services/categoryService";
 import PageTabs from '../components/PageTabs';
 
 const HistoryPage = () => {
@@ -19,17 +21,146 @@ const HistoryPage = () => {
     navigate("/order");
   };
 
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const resetMessages = () => {
+    setError(null);
+    setSuccess(false);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+    setSelectedCategory("");
+    setItems([]);
+    resetMessages();
+  };
+
+  const handleShow = () => setShow(true);
+
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [items, setItems] = useState([]);
+  
+  const fetchCategories = async () => {
+    try {
+      const data = await getAllCategories();
+      setCategories(data);
+    } catch (error) {
+      setError("Oops! We couldn't fetch the categories. Please try again.");
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const handleCategoryChange = (categoryId) => {
+    setSelectedCategory(categoryId);
+    const category = categories.find(category => category.id === parseInt(categoryId));
+    setItems(category ? category.items : []);
+  };
+
   return (
     <Container className="main-container py-3">
       <Stack direction="horizontal" gap={3} className="m-0 p-3">
-        <h2>{firstName ? firstName : "Guest's"}'s Orders</h2>
+        <h2>Activity</h2>
         <p className="ms-auto">{today}</p>
       </Stack>
 
       <Card className="p-2 mt-3">
         <Stack direction="horizontal" gap={3}>
-          <h3 className="m-0 p-2 fs-5 fw-bold">Order History</h3>
-          <Button variant="outline-primary" className="p-2 ms-auto" onClick={handleNavigate}>New Order</Button>     
+          <h3 className="m-0 p-2 fs-5 fw-bold">Inventory Log</h3>
+          <Button variant="outline-primary" className="p-2 ms-auto" onClick={handleShow}>New Log</Button>     
+          <Modal size="lg" centered show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Inventory Log</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Form id="inventoryLogForm">
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="itemCategory">
+                    <Form.Label>Category</Form.Label>
+                    <Form.Select
+                      value={selectedCategory}
+                      onChange={(e) => handleCategoryChange(e.target.value)}
+                      required
+                    >
+                      <option value="">Select...</option>
+                      {categories.map(category => (
+                        <option key={category.id} value={category.id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="itemName">
+                    <Form.Label>Item</Form.Label>
+                    <Form.Select
+                      name="itemId"
+                      disabled={!selectedCategory}
+                      required
+                    >
+                      {!selectedCategory ? (
+                        <option value="">Select a category</option>
+                      ) : items.length > 0 ? (
+                        <>
+                          <option value="">Select...</option>
+                          {items.map(item => (
+                            <option key={item.id} value={item.id}>
+                              {item.name}
+                            </option>
+                          ))}
+                        </>
+                      ) : (
+                        <option value="">No items available</option>
+                      )}
+                    </Form.Select>
+                  </Form.Group>
+                </Row>
+                
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="itemChangeQuantity">
+                    <Form.Label>Change Quantity</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="quantity"
+                      placeholder="Enter quantity"
+                    />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="itemChangeType">
+                    <Form.Label>Change Type</Form.Label>
+                    <Form.Select>
+                      <option value="">Select...</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Row>
+
+                <Form.Group className="mb-3" controlId="itemChangeReason">
+                  <Form.Label>Reason</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={2}
+                    name="description"
+                    placeholder="Enter description"
+                  />
+                </Form.Group>
+                {error && <div className='alert alert-danger'>{error}</div>}
+                {success && <div className='alert alert-success'>New item added successfully!</div>}
+              </Form>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" type="submit" form="addItemForm">
+                Submit
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Stack>
       </Card>
       
