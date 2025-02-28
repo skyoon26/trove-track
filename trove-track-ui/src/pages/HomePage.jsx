f62a187f436cd91ffd64a6f600bf7462b153e4d1
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-28 15:39:56 -0600

    Update Recent Activity in HomePage to display inventory logs dynamically

--- trove-track-ui/src/pages/HomePage.jsx
+++ trove-track-ui/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, Card, Stack, Button, Accordion, Row, Col, Tab, Tabs } from 'react-bootstrap';
+import { Container, Card, Stack, Button, Accordion, Row, Col, Table } from 'react-bootstrap';
 import { getAllCategories } from '../services/categoryService';
+import { getAllLogs } from '../services/inventoryLogService';
 import './pages.css';
 import PageTabs from '../components/PageTabs';
 import SummaryCard from '../components/SummaryCard';
@@ -19,6 +20,7 @@ const HomePage = () => {
   
   // Manages category state, fetches category data from API, and updates UI
   const [categories, setCategories] = useState([]);
+  const [logs, setLogs] = useState([]);
   
   const fetchCategories = async () => {
     try {
@@ -29,8 +31,18 @@ const HomePage = () => {
     }
   };
 
+  const fetchLogs = async () => {
+      try {
+        const data = await getAllLogs();
+        setLogs(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
   useEffect(() => {
     fetchCategories();
+    fetchLogs();
   }, []);
 
   const totalCategories = categories.length;
@@ -39,6 +51,8 @@ const HomePage = () => {
   categories.forEach(category => {
     totalItems += category.items?.length || 0;
   });
+
+  const recentLogs = logs.sort((a, b) => b.id - a.id).slice(0, 3);
 
   const navigate = useNavigate();
 
@@ -71,7 +85,7 @@ const HomePage = () => {
       <Card className="p-2 mt-4">
         <Stack direction="horizontal" gap={3}>
           <h3 className="m-0 p-2 fs-5 fw-bold">Low Stock Items</h3>
-          <Button variant="outline-primary" className="p-2 ms-auto" onClick={() => handleNavigate("/order")}>Order Item</Button>
+          <Button variant="outline-primary" className="p-2 ms-auto" onClick={() => handleNavigate("/activity")}>Update Inventory</Button>
         </Stack>
       </Card>
       <Accordion alwaysOpen className="pt-2">
@@ -116,34 +130,48 @@ const HomePage = () => {
       <Card className="p-2 mt-4">
         <Stack direction="horizontal" gap={3}>
           <h3 className="m-0 p-2 fs-5 fw-bold">Recent Activity</h3>
-          <Button variant="outline-primary" className="p-2 ms-auto" onClick={() => handleNavigate("/history")}>View History</Button>
+          <Button variant="outline-primary" className="p-2 ms-auto" onClick={() => handleNavigate("/activity")}>View All</Button>
         </Stack>
       </Card>
       <Accordion alwaysOpen className="pt-2">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
+        {recentLogs.length === 0 ? (
+          <Accordion.Item>
+            <Accordion.Header>No inventory logs added</Accordion.Header>
+          </Accordion.Item>
+        ) : (
+          recentLogs.map(log => (
+            <Accordion.Item
+              key={log.id}
+              eventKey={log.id.toString()}
+            >
+              <Accordion.Header>{log.id}</Accordion.Header>
+              <Accordion.Body>
+                <Table striped bordered hover responsive>
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>Item Name</th>
+                      <th>Quantity Changed</th>
+                      <th>Change Type</th>
+                      <th>Employee</th>
+                      <th>Reason</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr key={log.id}>
+                      <td>{log.changeDate}</td>
+                      <td>{log.itemName}</td>
+                      <td>{log.quantityChanged}</td>
+                      <td>{log.changeType}</td>
+                      <td>ID {log.changedByUserId}</td>
+                      <td>{log.reason}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))
+        )}
       </Accordion>
 
       <PageTabs />
